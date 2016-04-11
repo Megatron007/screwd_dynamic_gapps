herrmannw22/banks_dynamic_gapps

use custom keys to sign zips
Change-Id: Iccdd8783ad981cd5f1e682ea22130e2226393095

mkgapps.sh
@@ -96,14 +96,14 @@ for dirs in $APPDIRS; do
 done
 
 cd "$GAPPSDIR"
-7za a -tzip -r "$ZIPNAME1" ./* > /dev/null
+7za a -tzip -r "$ZIPNAME1" ./* 1> /dev/null 2>&1
 mv -f "$ZIPNAME1" "$TOOLSDIR"
 cd "$TOOLSDIR"
-java -Xmx2048m -jar signapk.jar -w testkey.x509.pem testkey.pk8 "$ZIPNAME1" "$ZIPNAME1".signed
+java -Xmx2048m -jar signapk.jar -w certificate.pem key.pk8 "$ZIPNAME1" "$ZIPNAME1".signed
 rm -f "$ZIPNAME1"
-zipadjust "$ZIPNAME1".signed "$ZIPNAME1".fixed > /dev/null
+zipadjust "$ZIPNAME1".signed "$ZIPNAME1".fixed 1> /dev/null 2>&1
 rm -f "$ZIPNAME1".signed
-java -Xmx2048m -jar minsignapk.jar testkey.x509.pem testkey.pk8 "$ZIPNAME1".fixed "$ZIPNAME1"
+java -Xmx2048m -jar minsignapk.jar certificate.pem key.pk8 "$ZIPNAME1".fixed "$ZIPNAME1"
 rm -f "$ZIPNAME1".fixed
 mv -f "$ZIPNAME1" "$FINALDIR"
 cp -f "$FINALDIR"/"$ZIPNAME1" "$FINALDIR"/"$ZIPNAME2"

Megatron007/screwd_dynamic_gapps

mkgapps.sh
@@ -85,14 +85,14 @@ for dirs in $APPDIRS; do
 done
 
 cd "$GAPPSDIR"
-7za a -tzip -r "$ZIPNAME" ./* > /dev/null
+7za a -tzip -r "$ZIPNAME" ./* 1> /dev/null 2>&1
 mv -f "$ZIPNAME" "$TOOLSDIR"
 cd "$TOOLSDIR"
-java -Xmx2048m -jar signapk.jar -w testkey.x509.pem testkey.pk8 "$ZIPNAME" "$ZIPNAME".signed
+java -Xmx2048m -jar signapk.jar -w certificate.pem key.pk8 "$ZIPNAME" "$ZIPNAME".signed
 rm -f "$ZIPNAME"
-zipadjust "$ZIPNAME".signed "$ZIPNAME".fixed > /dev/null
+zipadjust "$ZIPNAME".signed "$ZIPNAME".fixed 1> /dev/null 2>&1
 rm -f "$ZIPNAME".signed
-java -Xmx2048m -jar minsignapk.jar testkey.x509.pem testkey.pk8 "$ZIPNAME".fixed "$ZIPNAME"
+java -Xmx2048m -jar minsignapk.jar certificate.pem key.pk8 "$ZIPNAME".fixed "$ZIPNAME"
 rm -f "$ZIPNAME".fixed
 mv -f "$ZIPNAME" "$FINALDIR"
 