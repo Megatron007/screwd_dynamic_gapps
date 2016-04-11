herrmannw22/banks_dynamic_gapps

tools: go back to signing zips with jar file
* also add blank updater-script
* add updated compiled signapk.jar
* add zipadjust to fix signing errors
* add minsigapk.jar
* zip with p7zip
* adjust readme

Change-Id: Id9ee8fa776b8bc015397cb196b88155ed4bdea05

mkgapps.sh
@@ -72,7 +72,6 @@ ZIPNAME1="$ZIPNAME1TITLE"_"$ZIPNAME1VERSION"_"$ZIPNAME1DATE".zip
 ZIPNAME2="$ZIPNAME2TITLE"_"$ZIPNAME2VERSION".zip
 
 dcapk() {
-export PATH=$TOOLSDIR:$PATH
 TARGETDIR=$(realpath .)
 TARGETAPK=$TARGETDIR/$(basename "$TARGETDIR").apk
   unzip -q -o "$TARGETAPK" -d "$TARGETDIR" "lib/*"
@@ -89,15 +88,23 @@ TARGETAPK=$TARGETDIR/$(basename "$TARGETDIR").apk
 BEGIN=$(date +%s)
 
 # Begin the magic
+export PATH=$TOOLSDIR:$PATH
+
 for dirs in $APPDIRS; do
   cd "$GAPPSDIR/${dirs}";
   dcapk 1> /dev/null 2>&1;
 done
+
 cd "$GAPPSDIR"
-zip -q -r -9 "$ZIPNAME1" ./*
+7za a -tzip -r "$ZIPNAME1" ./* > /dev/null
 mv -f "$ZIPNAME1" "$TOOLSDIR"
 cd "$TOOLSDIR"
-./signapk.sh -q sign "$ZIPNAME1"
+java -Xmx2048m -jar signapk.jar -w testkey.x509.pem testkey.pk8 "$ZIPNAME1" "$ZIPNAME1".signed
+rm -f "$ZIPNAME1"
+zipadjust "$ZIPNAME1".signed "$ZIPNAME1".fixed > /dev/null
+rm -f "$ZIPNAME1".signed
+java -Xmx2048m -jar minsignapk.jar testkey.x509.pem testkey.pk8 "$ZIPNAME1".fixed "$ZIPNAME1"
+rm -f "$ZIPNAME1".fixed
 mv -f "$ZIPNAME1" "$FINALDIR"
 cp -f "$FINALDIR"/"$ZIPNAME1" "$FINALDIR"/"$ZIPNAME2"
 

Megatron007/screwd_dynamic_gapps

mkgapps.sh
@@ -61,7 +61,6 @@ ZIPNAMEDATE=$(date +%-m-%-e-%-y)
 ZIPNAME="$ZIPNAMETITLE"_"$ZIPNAMEVERSION"_"$ZIPNAMEDATE".zip
 
 dcapk() {
-export PATH=$TOOLSDIR:$PATH
 TARGETDIR=$(realpath .)
 TARGETAPK=$TARGETDIR/$(basename "$TARGETDIR").apk
   unzip -q -o "$TARGETAPK" -d "$TARGETDIR" "lib/*"
@@ -78,15 +77,23 @@ TARGETAPK=$TARGETDIR/$(basename "$TARGETDIR").apk
 BEGIN=$(date +%s)
 
 # Begin the magic
+export PATH=$TOOLSDIR:$PATH
+
 for dirs in $APPDIRS; do
   cd "$GAPPSDIR/${dirs}";
   dcapk 1> /dev/null 2>&1;
 done
+
 cd "$GAPPSDIR"
-zip -q -r -9 "$ZIPNAME" ./*
+7za a -tzip -r "$ZIPNAME" ./* > /dev/null
 mv -f "$ZIPNAME" "$TOOLSDIR"
 cd "$TOOLSDIR"
-./signapk.sh -q sign "$ZIPNAME"
+java -Xmx2048m -jar signapk.jar -w testkey.x509.pem testkey.pk8 "$ZIPNAME" "$ZIPNAME".signed
+rm -f "$ZIPNAME"
+zipadjust "$ZIPNAME".signed "$ZIPNAME".fixed > /dev/null
+rm -f "$ZIPNAME".signed
+java -Xmx2048m -jar minsignapk.jar testkey.x509.pem testkey.pk8 "$ZIPNAME".fixed "$ZIPNAME"
+rm -f "$ZIPNAME".fixed
 mv -f "$ZIPNAME" "$FINALDIR"
 
 # Define ending time