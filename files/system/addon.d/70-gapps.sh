b13edb209993985269f798cb9891c19e620bc36e
MrBaNkS <banksmi09@gmail.com>
2016-04-14 05:35:53 -0500

    scripts: redo
    * use sdk version to base compatibility
    * tighten up grepping
    * make operation more verbose
    * shut up permission printing
    * use some functions from SuperSU zip to mount system for more compatibilty
    * borrow some stuff from Open GApps. They are scripting geniouses. Add back GNU licenses to reflect back
    
    Change-Id: Ib75a08b0b7de9a34390be066641037e5c86078f6

--- files/system/addon.d/70-gapps.sh
+++ files/system/addon.d/70-gapps.sh
@@ -4,11 +4,27 @@
 # /system/addon.d/70-gapps.sh
 #
 
+# This file contains parts from the scripts taken from the Open GApps Project by mfonville.
+#
+# The Open GApps scripts are free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# These scripts are distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
 # Execute
 . /tmp/backuptool.functions
 
 # Functions & variables
-arch=$(cat /system/build.prop | grep -m 1 "ro.product.cpu.abi=")
+file_getprop() { grep "^$2" "$1" | cut -d= -f2; }
+
+rom_build_prop=/system/build.prop
+
+arch=$(file_getprop $rom_build_prop "ro.product.cpu.abi")
 
 list_files() {
 cat <<EOF
